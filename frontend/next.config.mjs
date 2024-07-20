32b12313c2ebc1c4c944758d8a634e362cfb4644
chmaster <smart.tact.cheeser.0803@gmail.com>
2024-07-20 17:48:35 +0900

    arrage folder

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
             {
                 source: '/',
                 destination: '/Main',
-                permanent: true,
+                permanent: false,
             },
         ];
     }
