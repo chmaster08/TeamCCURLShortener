45f5f8b9e045279dc11b4d580e84eec0dc270f7a
yaaasuu <anyongw01123@gmail.com>
2024-07-23 23:27:51 +0900

    management pege追加

---
+++ frontend/src/features/management/EditDialog.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, InputAdornment } from "@mui/material";
+import { LinkData } from "./Links"
+
+interface DialogsProps {
+  openEditDialog: boolean;
+  editedName: string;
+  editedShortUrl: string;
+  link: LinkData | null;
+  setEditedName: (name:string) => void;
+  setEditedShortUrl: (shortUrl: string) => void;
+  handleEditDialogClose: () => void;
+  handleEditSave: () => void;
+}
+
+export default function EditDialog({ openEditDialog, editedName, editedShortUrl, link, setEditedName, setEditedShortUrl, handleEditDialogClose, handleEditSave }: DialogsProps) {
+  return (
+    <Dialog open={openEditDialog} onClose={handleEditDialogClose}>
+      <DialogTitle>Edit Link</DialogTitle>
+      <DialogContent>
+        <TextField
+        autoFocus
+        margin="dense"
+        label="Name"
+        fullWidth
+        variant="standard"
+        value={editedName}
+        onChange={(e) => setEditedName(e.target.value)}
+        />
+        <TextField
+          margin="dense"
+          label="Short URL"
+          fullWidth
+          variant="standard"
+          value={editedShortUrl}
+          onChange={(e) => setEditedShortUrl(e.target.value)}
+          InputProps={{
+          startAdornment: <InputAdornment position="start">{link?.shortUrl.split("/")[0]}/</InputAdornment>,
+          endAdornment: <InputAdornment position="end">/</InputAdornment>,
+          }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleEditDialogClose}>Cancel</Button>
+        <Button onClick={handleEditSave}>Save</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
---
+++ frontend/src/features/management/Links bk1.tsx
@@ -0,0 +1,187 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Avatar, Box, Button, Dialog, DialogActions, DialogTitle, DialogContent, DialogContentText, TextField, InputAdornment, IconButton, Link, Paper, Stack, Typography } from "@mui/material";
+import CopyIcon from "@mui/icons-material/ContentCopy";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import QrCodeIcon from "@mui/icons-material/QrCode";
+import EditDialog from "./EditDialog";
+import DeleteDialog from "./DeleteDialog";
+import Alerts from "./Alerts";
+
+export interface LinkData {
+  id: number;
+  name: string;
+  url: string;
+  shortUrl: string;
+}
+
+const fetchLinksData = async (): Promise<LinkData[]> => {
+  return [
+    { id: 1, name: "TeamCC URLShortener", url: "https://github.com/chmaster08/TeamCCURLShortener", shortUrl: "teamcc.com/ab123" },
+    { id: 2, name: "TeamAA URLShortener", url: "https://github.com/chmaster08/TeamCCURLShortener", shortUrl: "teamaa.com/ab123" },
+    { id: 3, name: "TeamBB URLShortener", url: "https://github.com/chmaster08/TeamCCURLShortener", shortUrl: "teambb.com/ab123" },
+    { id: 4, name: "TeamAA URLShortener", url: "https://github.com/chmaster08/TeamCCURLShortener", shortUrl: "teamaa.com/ab123" },
+    { id: 5, name: "TeamBB URLShortener", url: "https://github.com/chmaster08/TeamCCURLShortener", shortUrl: "teambb.com/ab123" },
+    { id: 6, name: "TeamAA URLShortener", url: "https://github.com/chmaster08/TeamCCURLShortener", shortUrl: "teamaa.com/ab123" },
+    { id: 7, name: "TeamBB URLShortener", url: "https://github.com/chmaster08/TeamCCURLShortener", shortUrl: "teambb.com/ab123" },
+  ];
+};
+
+export default function Links() {
+  const [linksData, setLinksData] = useState<LinkData[]>([]);
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const [openEditDialog, setOpenEditDialog] = useState(false);
+  const [editingLink, setEditingLink] = useState<LinkData | null>(null);
+  const [editedName, setEditedName] = useState("");
+  const [editedShortUrl, setEditedShortUrl] = useState("");
+  const [deletedAlertOpen, setDeletedAlertOpen] = useState(false);
+  const [copiedAlertOpen, setCopiedAlertOpen] = useState(false);
+  const [failedAlert, setFailedAlert] = useState(false);
+
+  useEffect(() => {
+    const loadLinksData = async () => {
+      const data = await fetchLinksData();
+      setLinksData(data);
+    };
+
+    loadLinksData();
+  }, []);
+
+  const handleDeleteDialogOpen = () => {
+    setOpenDeleteDialog(true);
+  };
+
+  const handleDeleteDialogClose = () => {
+    setOpenDeleteDialog(false);
+  };
+
+  const handleDelete = () => {
+    // TODO:DBからの削除処理
+    setOpenDeleteDialog(false);
+    setDeletedAlertOpen(true);
+    setTimeout(() => setDeletedAlertOpen(false), 3000);
+  };
+
+  const handleCopyLink = (url:string) => {
+    navigator.clipboard.writeText("https://github.com/chmaster08/TeamCCURLShortener")
+    .then(() => {
+      setCopiedAlertOpen(true);
+      setTimeout(() => setCopiedAlertOpen(false), 3000);
+    })
+    .catch(() => {
+      setFailedAlert(true);
+      setTimeout(() => setFailedAlert(false), 3000);
+    });
+  };
+
+  const handleOpenEditDialog = (link: LinkData) => {
+    setEditingLink(link);
+    setEditedName(link.name);
+    setEditedShortUrl(link.shortUrl.split("/").pop() || "");
+    setOpenEditDialog(true);
+  };
+
+  const handleEditDialogClose = () => {
+    setOpenEditDialog(false);
+    setEditingLink(null);
+  };
+
+  const handleEditSave = () => {
+    // TODO:DBの更新処理
+    setOpenEditDialog(false);
+    setEditingLink(null);
+  };
+
+  if (linksData.length === 0){
+    return (
+      <Paper elevation={3} sx={{ position: "relative", p: 9, mb: 4 }}>
+        <Box sx={{ position: "absolute", top: "30%", left: "40%" }}>
+          <Typography variant="h6">you do not have any links.</Typography>
+        </Box>
+      </Paper>
+    );
+  } else{
+    return (
+      linksData.map((link) => (
+        <Paper key={link.id} elevation={3} sx={{ position: "relative", p: 9, mb: 4 }}>
+          <Box sx={{ position: "absolute", top: 8, right: 8 }}>
+            <IconButton aria-label="copy" onClick={() => handleCopyLink(link.url)}>
+              <CopyIcon />
+            </IconButton>
+            <IconButton aria-label="edit" onClick={() => handleOpenEditDialog(link)}>
+              <EditIcon />
+            </IconButton>
+            <IconButton aria-label="delete" onClick={handleDeleteDialogOpen}>
+              <DeleteIcon />
+            </IconButton>
+            <IconButton aria-label="qr code">
+              <QrCodeIcon />
+            </IconButton>
+          </Box>
+          <Box sx={{ position: "absolute", top: 27, left: 20 }}>
+            <Stack direction="row" spacing={2}>
+              <Avatar alt={link.name} src={`http://www.google.com/s2/favicons?domain=${link.url}&size=100`} />
+              <Typography variant="h5" gutterBottom>
+                {link.name}
+              </Typography>
+            </Stack>
+          </Box>
+          <Box mt={4}>
+            <Link href={link.url} variant="h6" gutterBottom underline="hover">
+              {link.shortUrl}
+            </Link>
+            <Box>
+              <Link href={link.url} variant="body1" gutterBottom underline="hover" color="inherit">
+                {link.url}
+              </Link>
+            </Box>
+          </Box>
+          {/* <EditDialog
+              openEditDialog={openEditDialog}
+              handleEditDialogClose={handleEditDialogClose}
+              handleEditSave={handleEditSave}
+              link={editingLink}
+              editedName={editedName}
+              editedShortUrl={editedShortUrl}
+              setEditedName={setEditedName}
+              setEditedShortUrl={setEditedShortUrl}
+            />
+            <DeleteDialog
+              openDeleteDialog={openDeleteDialog}
+              handleDeleteDialogClose={handleDeleteDialogClose}
+              handleDelete={handleDelete}
+            /> */}
+            <Dialog
+              open={openDeleteDialog}
+              onClose={handleDeleteDialogClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">{"Delete link?"}</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  This cannot be undone.
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleDeleteDialogClose} color="primary">
+                  cancel
+                </Button>
+                <Button onClick={handleDelete} sx={{ color: "red"}} autoFocus>
+                  delete
+                </Button>
+              </DialogActions>
+            </Dialog>
+            <Alerts
+              copiedAlertOpen={copiedAlertOpen}
+              deletedAlertOpen={deletedAlertOpen}
+              failedAlert={failedAlert}
+            />
+        </Paper>
+      ))
+    );
+  }
+
+}
