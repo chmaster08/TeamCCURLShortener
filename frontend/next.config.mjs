e076ba7397a7a95cf80a21cd31c289b71de0e630
chmaster <smart.tact.cheeser.0803@gmail.com>
2024-08-30 00:55:38 +0900

    for pages

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:"export",
+    assetPrefix: './',
+    trailingSlash: true,
+    images: {
+    unoptimized: true,
+    },
 };
+const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '');
 
+// 本番環境（GitHub Pages）でのみbasePath設定
+if (process.env.GITHUB_ACTIONS) {
+  nextConfig.basePath = `/${repo}`;
+}
 export default nextConfig;
